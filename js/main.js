098777c1b25df63da96931db61761e7dcebf2694
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2024-01-07 20:59:02 +0200

    UI update & links functionality

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 window.addEventListener("scroll", function () {
   if (window.scrollY / 1000 > 0.1) {
-    $("header").addClass("shadow-custom");
     $("header").addClass("scrolled");
   } else {
-    $("header").removeClass("shadow-custom");
     $("header").removeClass("scrolled");
   }
 });
 
 window.addEventListener("load", function () {
   if (window.scrollY / 1000 > 0.1) {
-    $("header").addClass("shadow-custom");
     $("header").addClass("scrolled");
   } else {
-    $("header").removeClass("shadow-custom");
     $("header").removeClass("scrolled");
   }
 });
